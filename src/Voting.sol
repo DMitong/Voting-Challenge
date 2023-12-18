ec6c56759d3149dd8c522341fe2842a8ac5abda5
DMitong <mitongdapal@gmail.com>
2023-12-18 19:30:59 +0100

    Initialised Project

---
+++ src/Voting.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+// Challenge: This is part of a 15 day smart contract writing challenge, with 5 days for each level of difficulty, basic, intermediate and advanced.
+
+/*
+Basic 3: 
+Write a smart contract that can implement a simple voting system.
+The contract should have an array that stores the candidates’ names. 
+The contract should also have a mapping that stores the votes for each candidate.
+The contract should also have a function to allow voters to cast their votes, only once per address.
+The contract should also have a function to declare the winner, based on the highest number of votes.
+*/
+
+contract Voting {
+
+}
